eb26a8d6b30e8ba3172dab24b112ba61b745d792
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-01 14:14:24 +0530

    added middlewares

---
+++ apps/api/src/middlewares/error.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandler(
+    err: any,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) {
+    console.error(err);
+    res.status(400).json({
+        error: err.message ?? "Something went wrong",
+    });
+}
