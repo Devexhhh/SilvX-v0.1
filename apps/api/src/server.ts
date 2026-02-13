12759ae577fd59b2bb45f68a3f32125899644832
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-13 23:44:03 +0530

    error handling in backend

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,10 @@ import app from "./app";
 
 const PORT = 4000;
 
+process.on("unhandledRejection", (err: any) => {
+    console.error("UNHANDLED REJECTION:", err);
+});
+
 app.listen(PORT, () => {
     console.log(`API running on http://localhost:${PORT}`);
 });
