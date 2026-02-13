d3ac53fae18e84e280cfc8ec4f6658c3e6f7ebb0
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-13 21:47:34 +0530

    v1 for testing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,57 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+"use client";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+import { useState } from "react";
+import { apiPost } from "../lib/api";
+import BalanceCard from "../components/BalanceCard";
+import TradeForm from "../components/TradeForm";
+import TransactionHistory from "../components/TransactionHistory";
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [userId, setUserId] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.dev/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.dev?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.dev →
-        </a>
-      </footer>
-    </div>
+  async function createUser() {
+    const res = await apiPost("/users", {
+      phone: "9999999999",
+    });
+
+    setUserId(res.id);
+  }
+
+  async function deposit() {
+    await apiPost("/deposits", {
+      userId,
+      amount: 1000,
+      referenceId: `DEP_${Date.now()}`,
+    });
+
+    alert("Deposited ₹1000");
+  }
+
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Digital Silver</h1>
+
+      {!userId && (
+        <button onClick={createUser}>Create User</button>
+      )}
+
+      {userId && (
+        <>
+          <p>User ID: {userId}</p>
+
+          <button onClick={deposit}>
+            Deposit ₹1000
+          </button>
+
+          <BalanceCard userId={userId} />
+
+          <TradeForm userId={userId} type="buy" />
+          <TradeForm userId={userId} type="sell" />
+          <TransactionHistory userId={userId} />
+
+        </>
+      )}
+    </main>
   );
 }
